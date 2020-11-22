4ee51ad2a30caf56b4b923a5eb77dd50a84882a5
iamrivera <a3m3rivera@gmail.com>
2020-11-21 22:44:36 -0800

    Url Array + Country Class

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,10 +3,9 @@ module LatinRootsII
     class CLI
        
         def call
-            puts "Hello World, Testing"
-            page = Nokogiri::HTML(open("https://www.britannica.com/topic/list-of-countries-in-Latin-America-2061416"))
-            page.css("ul.topic-list li div a[href]").collect { |link| link['href']}   
-            binding.pry        
+            puts "Hello World, Testing" 
+            country_urls = LatinRootsII::Scraper.new.class.all 
+            binding.pry
         end
 
     end
--- lib/scaper.rb
+++ lib/scaper.rb
@@ -1,24 +1,22 @@
-# module LatinRootsII
+module LatinRootsII
 
-#     class Scraper 
-#         attr_accessor :name, :name_official, :government, :leader, :capital, :language, :currency, :intro #try dynamic attr function (see learn module notes on "dynamic attr" for code syntax)
+    class Scraper 
+        attr_accessor :name, :name_official, :government, :leader, :capital, :language, :currency, :intro #try dynamic attr function (see learn module notes on "dynamic attr" for code syntax)
 
-#         @@all = []
+        @@all = [] #provides me with an array of all the country urls
 
-#         def initialize 
-#             # source = "https://www.britannica.com/topic/list-of-countries-in-Latin-America-2061416" #Consider breaking url apart (i.e. main and slug)
-#             page = Nokogiri::HTML(open("https://www.britannica.com/topic/list-of-countries-in-Latin-America-2061416"))
-#             country_urls = page.css("ul.topic-list li div a[href]").collect { |link| link['href']}
-#         end
+        def initialize 
+            # source = "https://www.britannica.com/topic/list-of-countries-in-Latin-America-2061416" #Consider breaking url apart (i.e. main and slug)
+            page = Nokogiri::HTML(open("https://www.britannica.com/topic/list-of-countries-in-Latin-America-2061416"))
+            country_urls = page.css("ul.topic-list li div a[href]").collect { |link| link['href']}
+            country_urls.each {|url| @@all << url}
+        end
 
-#         def self.all
-#             @@all
-#         end
+        def self.all
+            @@all
+        end
         
-
-#     end
-
+    end
 
 
-
-# end
+end
